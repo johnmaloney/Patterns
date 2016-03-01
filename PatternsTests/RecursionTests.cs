86a0e599469c4e4930a40df95978bdd48a09e559
johnmaloney <maloney1@gmail.com>
2016-03-01 03:40:39 +0000

    Tower of Brahma problem displaying simple recursion

---
+++ PatternsTests/RecursionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Patterns;
+using PatternsTests;
+
+namespace PatternsTests
+{
+    [TestClass]
+    public class RecursionTests
+    {
+        [TestMethod]
+        public void create_hanoi_tower_expect_rules()
+        {
+
+            //       =1           =       =
+            //      =2=           =       =
+            //     ==3==          =       = 
+            //    ===4===         =       =
+            //   ====5====        =       =
+            //=================================
+
+            var amountOfDisks = 8;
+
+            var tower = new HanoiTower();
+
+            var source = new Stack<int>().AddRange(amountOfDisks);
+            var destination = new Stack<int>();
+            var temporary = new Stack<int>();
+
+            tower.MoveTower(source.Count, source, destination, temporary);
+
+            Assert.AreEqual(0, source);
+            Assert.AreEqual(amountOfDisks, destination);
+            Assert.AreEqual(0, temporary);
+        }
+    }
+
+   
+}
---
+++ PatternsTests/TestExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsTests
+{
+    public static class TestExtensions
+    {
+        public static Stack<int> AddRange(this Stack<int> source, int amountToAdd)
+        {
+            for (int i = amountToAdd; i > 0; i--)
+            {
+                source.Push(i);
+            }
+            return source;
+        }
+    }
+}
