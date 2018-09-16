215eeea6d926689d1d954ab8e13fbbd6aed07c56
John Maloney <maloney1@hotmail.com>
2018-09-16 18:07:17 +0000

    Adding the O(1) notation and geospatial items

---
+++ Algorithms/Algorithms/Models/PointSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Models
+{
+    /// <summary>
+    /// Using Latitude and Longitude for the Y and the X respectively.
+    /// </summary>
+    public class PointSystem
+    {
+        public IEnumerable<Point> NorthAmerica()
+        {
+
+        }
+    }
+}
---
+++ Algorithms/Algorithms/ObyOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Represents the base concept of O(1) notation. 
+    /// This is the simplest form of O notation and scales perfectly because it will always take the
+    /// first item in the collection.
+    /// 
+    /// 
+    /// </summary>
+    public class ObyOne
+    {
+        public static float Find()
+        {
+
+        }
+    }
+}
